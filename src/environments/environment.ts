ce3fa99d6a58f08ea35dc1c93ee246530e1ab7ff
lucasvasconcelo78 <lucas.vasconcelos@b2mlportal.com.br>
2022-01-26 00:20:39 -0300

    Configurando o projeto, para começar a produzir

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,23 @@
 
 export const environment = {
   production: false
+
+};
+
+export const SpotifyConfiguration = {
+  clientId: '796d79cd1e6c421ba7b5838962769a39',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", // musica tocando agora.
+    "user-read-recently-played", // ler musicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuario
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alterar do player do usuario.
+    "user-library-read", // ler biblioteca dos usuarios
+    "playlist-read-private", // ler playlists privads
+    "playlist-read-collaborative" // ler playlists colaborativas
+  ]
 };
 
 /*
