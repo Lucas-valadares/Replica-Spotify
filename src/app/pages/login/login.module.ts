0fb142fb6fdbffb5060914b4b93b1399152bfe1a
lucasvasconcelo78 <lucas.vasconcelos@b2mlportal.com.br>
2022-01-27 16:37:02 -0300

    criado a tela de login, e pedindo autorização no próprio Spotify
    Criação da tela de login, juntamente com o componente e as services dele.

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { LoginComponent } from './login.component';
+import { RouterModule } from '@angular/router';
+import { LoginRotas } from './login.routes';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(LoginRotas)
   ]
 })
+
 export class LoginModule { }
