5bc010238f88a5a142a9a98dd24f6cdb81f266d6
lucasvasconcelo78 <lucas.vasconcelos@b2mlportal.com.br>
2022-03-29 11:53:35 -0300

    Criando o menu Lateral
    Criando o menu lateral e inplementando  a logica para o botão mudar de cor quando selecionado

--- src/app/components/painel-esquerdo/painel-esquerdo.component.ts
+++ src/app/components/painel-esquerdo/painel-esquerdo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faGuitar, faHome, faMusic, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-painel-esquerdo',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./painel-esquerdo.component.scss']
 })
 export class PainelEsquerdoComponent implements OnInit {
+  menuSelecionado = 'Home'
+
+  //icones
+  homeIcone = faHome;
+  pesquisarIcone = faSearch;
+  artistaIcone = faGuitar;
+  playlist = faMusic;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  botaoClick(botao: string){
+    this.menuSelecionado = botao;
+  }
 }
